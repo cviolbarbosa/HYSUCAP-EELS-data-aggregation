0cce4688ec58a24a09948f7946aa01c8397da12f
EdSci <carlos@DESKTOP-PVKD3T9.localdomain>
2021-11-13 18:53:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+##  Required imports
+import matplotlib; matplotlib.use('Agg')
+import sdesk
+import os
+from data_transformation import aggregate_to_sample_data, convert_data_layers_to_img, GAF_transform
+import numpy as np
+from matplotlib import pyplot as plt
+import hyperspy.api as hs
+hs.preferences.GUIs.enable_traitsui_gui = False
+hs.preferences.GUIs.enable_ipywidgets = False
+hs.preferences.GUIs.warn_if_guis_are_missing = False
+##
+
+
+
+# Define your method main()
+def main():
+    global global_deltas_ke 
+    process = sdesk.Process()
+
+    # LOAD INPUT FILES OR SAMPLES
+    input_file = process.input_files[0] 
+    file_name, file_extension = os.path.splitext(input_file.properties['name']) 
+    if file_extension.lower() not in ['.msa']:
+        """ STOP AND DO NOTHING """
+        return 0
+    
+    # LOAD PARAMETERS FROM USER INPUT FORM
+    input_form = process.arguments 
+    
+    # PROCESS INPUT FILE AND EXTRACT DATA
+    s = hs.load(input_file.path)
+    x_axis = s.axes_manager[-1]
+    last_x = x_axis.offset + x_axis.scale*x_axis.size
+    x_np = np.linspace(x_axis.offset, last_x, x_axis.size )
+    xy_np = np.array([x_np,s.data]).transpose()
+    
+    # CREATE THUMBNAIL IMAGE
+    sdesk_thumbnail = process.create_output_file('_thumbnail_picture.png')
+    s.plot(); fig = plt.gcf() 
+    fig.savefig(sdesk_thumbnail.path)
+
+    # UPDATE CUSTOM PROPERTIES OF INPUT FILE
+    input_file.custom_properties.update(s.original_metadata.as_dictionary())
+    input_file.save_custom_properties()
+
+    # CREATE PARSED OUTPUT FILES
+    out_file = { 'name':f"{file_name.split('.')[0]}_exported.txt",
+                 'columns': ["energy loss","Electrons"],
+                 'data': xy_np,
+                 'header': sdesk.json_to_text(input_file.custom_properties)
+              }
+    
+    sdesk_output_file = process.create_output_file(out_file['name'])
+    sdesk.write_tsv_file(sdesk_output_file.path, out_file['columns'], out_file['data'],  out_file['header'])
+
+    
+    # Aggregate data to sample and output for visualization
+    linked_subject = input_file.subject
+    if linked_subject:
+        data_obj, path = aggregate_to_sample_data(linked_subject, [out_file['data'] ], 'eels')
+        linked_subject.save_as_aggregated_data(path)
+    
+        sdesk_output_file = process.create_output_file("aggregated_eels_table.txt")
+        sdesk.write_tsv_file(sdesk_output_file.path, ['energy', 'yield'], data_obj['eels'])
+        
+        gaf_eels, _, _ = GAF_transform(data_obj['eels'][:,1])
+
+        if data_obj.get('xps', None) is not None:
+            gaf_xps,  _, _ = GAF_transform(data_obj['xps'][:,1])
+        else:
+            gaf_xps = np.zeros(gaf_eels.shape)
+
+        img = convert_data_layers_to_img(gaf_xps, gaf_eels, gaf_eels*0)
+
+        aggregated_output_file = process.create_output_file(f"GAF_sample_{linked_subject.uid}.jpeg")
+        img.save(aggregated_output_file.path)
+
+
+
+# Call method main()
+main()
---
+++ support_files/sdesk.py
@@ -0,0 +1,129 @@
+import upserverlib
+import json
+from shutil import copyfile
+
+Support = upserverlib.Support
+
+
+class SdeskInputFile(upserverlib.InputFile):
+    """" Input class with methods to interact with Sdesk data manager """
+    def __init__(self, data, process):
+        super().__init__(data)
+        self.process = process
+        self.subject = None
+        file_subject = self.properties.get('subject', None)
+        if file_subject:
+            self.subject = SdeskInputSubject(file_subject, self.process)
+
+    def save_custom_properties(self):
+        output = self.process.get_or_create_output_file_update(self)
+        output.custom_properties.update(self.custom_properties)
+
+
+class SdeskInputSubject(upserverlib.InputSubject):
+    """" Input class with methods to interact with Sdesk data manager """
+    def __init__(self, data, process):
+        super().__init__(data)
+        self.process = process
+
+    def save_custom_properties(self):
+        output = self.process.get_or_create_output_subject_update(self, fields=["custom_properties"])
+        output.custom_properties.update(self.custom_properties)
+
+    def save_as_aggregated_data(self, input_path, name='aggregated_data'):
+        output = self.process.get_or_create_output_subject_update(self, fields=["aggregated_data"])
+        copyfile(input_path, output.aggregated_data.path)
+
+
+
+class InputItems:
+    def __init__(self, data_class,  process = None):
+        self._data_class = data_class
+        self._file_items = []
+        self.process = process
+
+    def add_item(self, data):
+        _type = data["type"]
+        self._file_items.append(self._data_class(data, self.process))
+
+    def __getitem__(self, index):
+        return self._file_items[index]
+
+
+
+class Process(upserverlib.Process):
+
+    def _get_input_files(self):
+        i = InputItems(process=self, data_class=SdeskInputFile)
+
+        with open(upserverlib.INPUT_FILESYSTEM) as fp:
+            rv = json.load(fp)
+            for x in rv.get("input", []):
+                if x["type"] == "file":
+                    i.add_item(x)
+            return i
+
+
+    def _get_input_subjects(self):
+        i =  InputItems(process=self, data_class=SdeskInputSubject)
+
+        with open(upserverlib.INPUT_FILESYSTEM) as fp:
+            rv = json.load(fp)
+            for x in rv.get("input", []):
+                if x["type"] == "subject":
+                    i.add_item(x)
+            return i
+
+
+
+
+def json_to_text(json_data):
+    text = "HEADER\n"
+    for key in json_data:
+        text += "{0}: {1}\n".format(key, json_data[key])
+    text += "\n"
+    return text
+
+
+def force_str(object):
+    try:
+        return str(object)
+    except:
+        pass
+    try:
+        return object.encode("utf-8")
+    except:
+        pass
+    try:
+        return object.encode("utf-8", "ignore").decode("utf-8")
+    except:
+        pass
+    try:
+        return object.encode("ascii", "ignore").decode("ascii")
+    except:
+        return ""
+
+
+def write_tsv_file(file_path, columns, data, pre_header=""):
+    """ Create tab separated value file from data rows. """
+    with open(file_path, "w") as fp:
+        fileposition = 0
+        if pre_header:
+            lines = pre_header.split("\n")
+            pre_header = "\n".join(["# " + line for line in lines]) + "\n\n"
+            fp.write(pre_header)
+            fileposition += len(pre_header)
+
+        # Header
+        column_names = ["[{0}]".format(col.replace("\n", " ")) for col in columns]
+        column_line = "\t".join(column_names) + "\n"
+        fp.write(column_line)
+        fileposition += len(column_line)
+        # Data
+        for row in data:
+            strings = [force_str(elem) for elem in row]
+            data_line = "\t".join(strings) + "\n"
+            fp.write(data_line)
+            fileposition += len(data_line)
+
+    return fileposition
